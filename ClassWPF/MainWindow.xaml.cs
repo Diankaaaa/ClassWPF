6b81df57d29fc3993b22a6737c0a8ec1881f648c
diana <diana@DESKTOP-AE9RS0A>
2021-01-12 16:38:41 +0300

    Добавьте файлы проекта.

---
+++ ClassWPF/MainWindow.xaml.cs
@@ -0,0 +1,55 @@
+﻿using ClassWPF.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClassWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            double real = double.Parse(a1.Text);
+            double imaginary = double.Parse(b1.Text);
+
+            Complex_number a = new Complex_number(real, imaginary);
+            OUTPUT.Text += "Модуль: " + a.ABC() + Environment.NewLine + "Обратное число: " + a.getReverseNumberA() + " " + a.getReverseNumberB() + Environment.NewLine + Environment.NewLine;
+        }
+
+        //Очистка поля вывода результата
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            OUTPUT.Text = String.Empty;
+        }
+
+        //Ввод только цифр
+        private void a1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(Char.IsDigit(e.Text, 0));
+        }
+
+        private void b1_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !(Char.IsDigit(e.Text, 0));
+        }
+    }
+}
---
+++ ClassWPF/Model/Complex_number.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassWPF.Model
+{
+    class Complex_number
+    {
+        private double a1; //Действительная часть числа
+        private double b1; //Мнимая часть числа
+
+        //Конструктор
+        public Complex_number (double real, double imaginary)
+        {
+            this.a1 = real;
+            this.b1 = imaginary;
+        }
+
+        public double A1
+        {
+            get { return a1; }
+            set { a1 = value; }
+        }
+
+        double B1
+        {
+            get { return b1; }
+            set { b1 = value; }
+        }
+
+        //Функция вычисления модуля
+        public double ABC()
+        {
+            return Math.Sqrt(a1 * a1 + b1 * b1);
+        }
+
+        //Функция получения обратного числа
+        public double getReverseNumberA()
+        {
+            double d = ((a1 * a1) + (b1 * b1));
+            return a1 / d;
+        }
+        public double getReverseNumberB()
+        {
+            double d = ((a1 * a1) + (b1 * b1));
+            return -b1 / d;
+        }
+    }
+}
